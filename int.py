6d1a028923872480254875d13922885093d6ddca
suisoh <fubuki.k016@gmail.com>
2022-05-02 15:25:19 +0800

    new

--- int.py
+++ int.py
@@ -12,3 +12,10 @@ g1 = Sample1("First Name", "Last Name", "Interest", 1234)
 print("Guest last name: " + g1.last)
 
 # Dictionary
+price_list = {"jeans": 255, "trousers": 149, "t-shirt": 69}
+price_list.update = ({"boots": 50, "sneakers": 370})
+price_list2 = {"Someting": 55}
+combined_price_list = zip(price_list, price_list2)
+
+# Using Dictionary
+
