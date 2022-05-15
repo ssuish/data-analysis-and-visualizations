3b897fbd76693ba5f5dc71207d12e7a3b1ad5d63
suisoh <fubuki.k016@gmail.com>
2022-05-15 15:46:23 +0800

    edit

--- stats-pandas.py
+++ stats-pandas.py
@@ -6,3 +6,28 @@ ser = pd.Series(f)
 sum_stat = ser.describe(f)
 
 print(sum_stat)
+
+# Population
+pstdev = ser.std(ddof=0)
+print(pstdev)
+
+# import and read csv
+# df = pd.read_csv("sample.csv")
+
+# display first 10 rows csv
+# frows = df.head(10)
+
+# display last 10 rows csv
+# lrows = df.tails(10)
+
+# row selection with conditions
+# sel = df[df["Country"] == "TW"]
+
+# filter countries using isin()
+# sea = df[df["Country"].isin(["Country 1", "Country 2", "Country 3"])]
+
+# summary of stats
+# stats = sea["Population (2020)"].describe()
+
+# ranking
+# sort_sea = sea.sort_values(by="population(2020)", ascending=false)
